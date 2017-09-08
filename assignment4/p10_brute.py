f8eb525e344a90854e1bd1d821b014ded1099abf
Isaac <isaacgoss14750@gmail.com>
2017-09-07 23:00:49 -0400

    p10 is kinda working, but I found another bug

--- assignment4/p10_brute.py
+++ assignment4/p10_brute.py
@@ -1,7 +1,37 @@
 
 MAX_RELEASE = 2
 MIN_LENGTH = 4
-MAX_LENGTH = 16
+MAX_LENGTH = 8
+
+def main(num_jobs):
+    jobs = gen_jobs(num_jobs)
+    
+    # I want to see what I started with.
+    print("==============================================")
+    print("=============== BEGINNING JOBS ===============")
+    print("==============================================")
+    print("release | length | time_run | time_left ")
+    for j in jobs:
+        print(j)
+    print("==============================================\n")
+
+    t = 0
+    while jobs:
+        runnable = get_runnable(jobs, t)
+        if runnable:
+            smallest_job = pick_sjf(runnable)
+            least_time = pick_srpt(runnable)
+            smallest_job.run()
+            if smallest_job is not least_time:
+                least_time.run() # These could be the same jobs! Don't run it twice!
+            print("release | length | time_run | time_left ")
+            print(smallest_job)
+            print(least_time)
+            print("======================================\n")
+        # else:
+        #     print("None runnable\n")
+        t += 1
+        jobs = check_done(jobs)
 
 class Job:
     def __init__(self, r, x):
@@ -10,10 +40,12 @@ class Job:
         self.time_run = 0
     def __len__(self):
         return self.length
-    def time_left():
+    def time_left(self):
         return self.length - self.time_run
-    def run():
+    def run(self):
         self.time_run += 1
+    def __repr__(self):
+        return str(self)
     def __str__(self):
         return "{} | {} | {} | {}".format(
             self.release,
@@ -53,22 +85,10 @@ def get_runnable(jobs, t):
             r.append(j)
     return r
 
-def main(num_jobs):
-    jobs = gen_jobs(num_jobs)
-    check_done = lambda jobs: (filter(lambda x: (x.time_left() > 0), jobs))
-    t = 0
-    while jobs:
-        runnable = get_runnable(jobs, t)
-        jobs = check_done(jobs)
-        if runnable:
-            smallest_job = pick_sjf(runnable)
-            least_time = pick_srpt(runnable)
-            smallest_job.run()
-            least_time.run()
-            print("release | length | time_run | time_left ")
-            print(smallest_job)
-            print(least_time)
-            print("======================================\n")
-        else:
-            print("None runnable\n")
-        t += 1
+def check_done(jobs):
+    nd = []
+    for j in jobs:
+        if j.time_left() > 0:
+            nd.append(j)
+    return nd
+
