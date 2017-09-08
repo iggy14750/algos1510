6f670c8989cccc590d21ab58e7fa30640ed4910f
Isaac <isaacgoss14750@gmail.com>
2017-09-07 23:18:03 -0400

    p10 seems to be basically operational

--- assignment4/p10_brute.py
+++ assignment4/p10_brute.py
@@ -1,37 +1,63 @@
 
-MAX_RELEASE = 2
+MAX_RELEASE = 12
 MIN_LENGTH = 4
-MAX_LENGTH = 8
+MAX_LENGTH = 32
+
+def deep_copy(jobs):
+    other = []
+    for j in jobs:
+        other.append(Job(j.release, j.length))
+    return other
 
 def main(num_jobs):
-    jobs = gen_jobs(num_jobs)
+    jobs_sm = gen_jobs(num_jobs)
+    jobs_ls = deep_copy(jobs_sm)
     
     # I want to see what I started with.
     print("==============================================")
     print("=============== BEGINNING JOBS ===============")
     print("==============================================")
     print("release | length | time_run | time_left ")
-    for j in jobs:
+    print("Smallest Job First:")
+    for j in jobs_sm:
+        print(j)
+    print("Smallest Remaining Processing Time:")
+    for j in jobs_ls:
         print(j)
     print("==============================================\n")
 
+    small_time = 0
+    least_time = 0
     t = 0
-    while jobs:
-        runnable = get_runnable(jobs, t)
-        if runnable:
-            smallest_job = pick_sjf(runnable)
-            least_time = pick_srpt(runnable)
-            smallest_job.run()
-            if smallest_job is not least_time:
-                least_time.run() # These could be the same jobs! Don't run it twice!
-            print("release | length | time_run | time_left ")
-            print(smallest_job)
-            print(least_time)
-            print("======================================\n")
-        # else:
-        #     print("None runnable\n")
+    while jobs_sm or jobs_ls:
+        if not jobs_sm:
+            print("Small won!\n")
+        else:
+            small_time += 1
+        if not jobs_ls:
+            print("Least won!\n")
+        else:
+            least_time += 1
+        runnable_sm = get_runnable(jobs_sm, t)
+        runnable_ls = get_runnable(jobs_ls, t)
+        print("release | length | time_run | time_left ")
+        if runnable_sm:
+            job = pick_sjf(runnable_sm)
+            job.run()
+            print(job)
+        if runnable_ls:
+            job = pick_srpt(runnable_ls)
+            job.run()
+            print(job)
+        print("======================================\n")
         t += 1
-        jobs = check_done(jobs)
+        jobs_sm = check_done(jobs_sm)
+        jobs_ls = check_done(jobs_ls)
+    print("==============================================")
+    print("=================== REPORT ===================")
+    print("==============================================")
+    print("Smallest Job First: {}".format(small_time))
+    print("Samllest Remaining Processing Time: {}".format(least_time))
 
 class Job:
     def __init__(self, r, x):
@@ -92,3 +118,5 @@ def check_done(jobs):
             nd.append(j)
     return nd
 
+if __name__ == '__main__':
+    main(4)
